a5b3709c18b49625708e53ce182761f22dbecf61
David Dubach <david.dubach96@gmail.com>
2021-10-22 17:59:32 +0200

    implement fit, predict, timer

--- task_1/solution.py
+++ task_1/solution.py
@@ -1,7 +1,9 @@
 import os
 import typing
+import time
+from functools import wraps
 
-from sklearn.gaussian_process.kernels import *
+from sklearn.gaussian_process.kernels import RBF, ConstantKernel, WhiteKernel
 import numpy as np
 from sklearn.gaussian_process import GaussianProcessRegressor
 from sklearn.model_selection import cross_val_score
@@ -11,7 +13,7 @@ from matplotlib import cm
 
 
 # Set `EXTENDED_EVALUATION` to `True` in order to visualize your predictions.
-EXTENDED_EVALUATION = False
+EXTENDED_EVALUATION = True
 EVALUATION_GRID_POINTS = 300  # Number of grid points used in extended evaluation
 EVALUATION_GRID_POINTS_3D = 50  # Number of points displayed in 3D during evaluation
 
@@ -21,6 +23,23 @@ THRESHOLD = 35.5
 COST_W_NORMAL = 1.0
 COST_W_OVERPREDICT = 5.0
 COST_W_THRESHOLD = 20.0
+
+
+def timer(start, end):
+    hours, rem = divmod(end-start, 3600)
+    minutes, seconds = divmod(rem, 60)
+    return "{:0>2}:{:0>2}:{:05.2f}".format(int(hours), int(minutes), seconds)
+
+
+def timefunc(func):
+    @wraps(func)
+    def wrap(*args, **kwargs):
+        ts = time.time()
+        result = func(*args, **kwargs)
+        te = time.time()
+        print(f"Function {func.__name__} took {timer(ts, te)}")
+        return result
+    return wrap
 
 
 class Model(object):
@@ -37,8 +56,14 @@ class Model(object):
         """
         self.rng = np.random.default_rng(seed=0)
 
-        # TODO: Add custom initialization for your model here if necessary
+        self.num_samples = 1000  # number of samples to draw from training set
 
+        kernel = 1*RBF(length_scale=0.2) + 1*RBF(length_scale=1) + \
+            1*WhiteKernel(noise_level=0.0001, noise_level_bounds=(1e-5, 1e-3))
+        self.model = GaussianProcessRegressor(kernel=kernel, n_restarts_optimizer=5)
+        print(f"Initial Kernel: {kernel}")
+
+    @timefunc
     def predict(self, x: np.ndarray) -> typing.Tuple[np.ndarray, np.ndarray, np.ndarray]:
         """
         Predict the pollution concentration for a given set of locations.
@@ -48,15 +73,14 @@ class Model(object):
             containing your predictions, the GP posterior mean, and the GP posterior stddev (in that order)
         """
 
-        # TODO: Use your GP to estimate the posterior mean and stddev for each location here
-        gp_mean = np.zeros(x.shape[0], dtype=float)
-        gp_std = np.zeros(x.shape[0], dtype=float)
+        gp_mean, gp_std = self.model.predict(x, return_std=True)
 
         # TODO: Use the GP posterior to form your predictions here
         predictions = gp_mean
 
         return predictions, gp_mean, gp_std
 
+    @timefunc
     def fit_model(self, train_x: np.ndarray, train_y: np.ndarray):
         """
         Fit your model on the given training data.
@@ -64,14 +88,18 @@ class Model(object):
         :param train_y: Training pollution concentrations as a 1d NumPy float array of shape (NUM_SAMPLES,)
         """
 
-        # TODO: Fit your model here
-        pass
+        # subsample of training set
+        idx = np.random.choice(len(train_y), self.num_samples, replace=False)
+        train_x_s = train_x[idx]
+        train_y_s = train_y[idx]
+
+        self.model.fit(train_x_s, train_y_s)
+        print(f"Optimized kernel: {self.model.kernel_}")
 
 
 def cost_function(y_true: np.ndarray, y_predicted: np.ndarray) -> float:
     """
     Calculates the cost of a set of predictions.
-
     :param y_true: Ground truth pollution levels as a 1d NumPy float array
     :param y_predicted: Predicted pollution levels as a 1d NumPy float array
     :return: Total cost of all predictions as a single float
@@ -173,7 +201,7 @@ def main():
     # Predict on the test features
     print('Predicting on test features')
     predicted_y = model.predict(test_x)
-    print(predicted_y)
+    # print(predicted_y)
 
     if EXTENDED_EVALUATION:
         perform_extended_evaluation(model, output_dir='.')
